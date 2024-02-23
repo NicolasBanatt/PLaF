e1ba9c3ed17c6b44f9500c2f86909460953b1ccb
Nicolas Banatt <nbanatt@stevens.edu>
2024-02-23 13:15:45 -0500

    new commit

--- src/let/lib/interp.ml
+++ src/let/lib/interp.ml
@@ -1,3 +1,9 @@
+(*
+Name: Nicolas Banatt
+Date: February 23, 2024
+Pledge: I pledge my honor that I have abided by the Stevens Honor System.
+*)
+
 open Parser_plaf.Ast
 open Parser_plaf.Parser
 open Ds
@@ -66,7 +72,51 @@ let rec eval_expr : expr -> exp_val ea_result =
     string_of_env >>= fun str ->
     print_endline str; 
     error "Debug called"
+  | EmptyList(_t) ->
+    return (ListVal [])
+  | Cons(e1,e2) ->
+    eval_expr e1 >>= fun h ->
+    eval_expr e2 >>= list_of_listVal >>= 
+    fun t -> return (ListVal (h::t))
+  | Hd(e) ->
+    eval_expr e >>=
+    list_of_listVal >>= fun l ->
+    if l = []
+    then error "Empty list!"
+    else return (List.hd l)
+  | Tl(e) ->
+    eval_expr e >>=
+    list_of_listVal >>= fun l ->
+    if l = [] 
+    then error "Empty list!"
+    else return (ListVal(List.tl l))
+  | IsEmpty(e) ->
+    eval_expr e >>=
+    list_of_listVal >>= fun l ->
+    (match l with
+    | [] -> return (BoolVal true)
+    | _ -> return (BoolVal false))
+  | Tuple(es) ->
+    eval_exprs es >>= fun tup ->
+    return (TupleVal(tup))
+  | Untuple(ids,e1,e2) ->
+    eval_expr e1 >>=
+    list_of_tupleVal >>= fun tup ->
+    if (List.length ids) = (List.length tup)
+    then 
+      extend_env_list ids tup >>+
+      eval_expr e2
+    else
+      error "Arguments do not match parameters!"
   | _ -> failwith "Not implemented yet!"
+and
+  eval_exprs : expr list -> ( exp_val list ) ea_result =
+  fun es ->
+  match es with
+  | [] -> return []
+  | h::t -> eval_expr h >>= fun i ->
+    eval_exprs t >>= fun l ->
+    return (i::l)
 
 (** [eval_prog e] evaluates program [e] *)
 let eval_prog (AProg(_,e)) =
